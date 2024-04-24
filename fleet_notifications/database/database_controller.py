13df24c1ef15fdc556b217f923e7986e3b3ec328
mario <mario.ivancik@bringauto.com>
2024-04-24 15:50:49 +0200

    use order states to determine notifications

--- fleet_notifications/database/database_controller.py
+++ fleet_notifications/database/database_controller.py
@@ -1,6 +1,22 @@
 from database.connection import get_connection_source
 
 
+def create_orders_table() -> None:
+    try:
+        with get_connection_source().begin() as conn:
+            conn.execute(
+                """
+                CREATE TABLE IF NOT EXISTS notification_orders (
+                    id SERIAL PRIMARY KEY,
+                    order_id INTEGER NOT NULL,
+                    
+                )
+                """
+            )
+    except:
+        pass
+
+
 def update_last_car_status(car_id: int, timestamp: int, status: str) -> None:
     try:
         with get_connection_source().begin() as conn:
